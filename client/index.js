940964c0352b095d405cf7e6d4347db0bc8c085d
Mordechai Katan <mordechai@sensory.co.il>
2023-02-21 17:38:31 +0200

    true first commit

---
+++ client/index.js
@@ -0,0 +1,40 @@
+
+const apiUrl = 'http://localhost:3000';
+let resultStatus; //boolean
+let resultData;
+
+
+const data =  {
+    sum: 1,
+    paymentsNum: 1,
+    description: "The destination of the payment",
+  }
+  
+const options = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+      },
+    body: JSON.stringify(data)
+}
+
+async function getPaymentLink (){
+   const response = await fetch(`${apiUrl}/api/Payment/GetPaymentLink`, options); //response in format {isSuccess: boolean, message: string (link or error-message)}
+   const data = await response.json();
+   console.log(data.message)
+   growPayment.renderPaymentOptions(data.message)
+}
+
+window.addEventListener("growWalletChange", (result) => {
+    console.log("result:", result.detail);
+    let res = result.detail;
+    if (res.status === 1) {
+      resultStatus = true;
+      resultData = res.data;
+    }
+    if (res.state === "close" && resultStatus) {
+      console.log(resultData) //or display success page whith resultData
+    }
+  });
+
+  //blabla
